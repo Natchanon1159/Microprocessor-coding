e69b4b37ae5214814ac3e34f442b398fd592c9b1
Natchanon1159 <60215208+Natchanon1159@users.noreply.github.com>
2020-02-20 15:07:16 +0700

    Add files via upload

---
+++ wk06_lab_ex2_delay.ino
@@ -0,0 +1,23 @@
+void setup()
+{
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+  Serial.println("START");
+}
+void loop()
+{
+  int counter;
+  for(counter =0; counter <= 120; counter++)
+  {
+    Serial.println(counter);
+
+    if(counter == 0 || counter <= 120)
+    {
+      digitalWrite(13,counter%2);
+      delay(1000);
+    }
+ }
+ Serial.print("STOP");
+ while(true);
+ 
+}
